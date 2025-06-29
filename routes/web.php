cae861bf0778a5f3fd943969cefd2f6a4c75b264
Raducu03 <mihnearaducu2019@gmail.com>
2025-06-29 20:14:14 +0300

    Day2 joi

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Rute pentru administratori
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::resource('posts', \App\Http\Controllers\Admin\PostController::class);
+        Route::resource('comments', \App\Http\Controllers\Admin\CommentController::class);
+        Route::patch('comments/{comment}/approve', [\App\Http\Controllers\Admin\CommentController::class, 'approve'])->name('comments.approve');
+    });
+
+    // Rute pentru comentarii
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
 });
+
+require __DIR__.'/auth.php';
